dadfc4b65fd43d4fb7ec7c82f98da56a6304b22d
Renaud Dumont <renaud.dumont@live.fr>
2015-09-18 09:08:03 +0200

    Nouveau projet bidon

---
+++ DummyApp/DummyApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DummyApp.Data;
+using DummyApp.Data.Model;
+
+namespace DummyApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Todos = new ObservableCollection<Todo>();
+        }
+
+        public async Task LoadData()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                Todos.Add(new Todo() { completed = true, id = 1, title = "My first todo", UserId = 1 });
+                Todos.Add(new Todo() { completed = true, id = 1, title = "My first todo", UserId = 1 });
+                Todos.Add(new Todo() { completed = true, id = 1, title = "My first todo", UserId = 1 });
+                Todos.Add(new Todo() { completed = true, id = 1, title = "My first todo", UserId = 1 });
+            }
+            else
+            {
+                var apiClient = new ApiClient();
+                var todos = await apiClient.GetTodos();
+                todos.ForEach(t => Todos.Add(t));
+            }
+        }
+
+        public ObservableCollection<Todo> Todos { get; set; }
+    }
+}
---
+++ DummyApp/DummyApp/Views/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using DummyApp.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DummyApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (sender, args) => { ViewModel = (MainViewModel)DataContext; };
+            this.Loaded += async (sender, args) => await ViewModel.LoadData();
+        }
+
+        public MainViewModel ViewModel { get; set; }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
